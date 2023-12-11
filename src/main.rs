124bb88cd71b9d00009f16adb6222c007ffc7e2a
cloudiful <45996018+cloudiful@users.noreply.github.com>
2023-12-11 17:39:16 +0800

    Moving codes to server.rs which makes main file so clean 😊

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use log::info;
 mod server;
 mod db;
 
@@ -11,22 +10,5 @@ fn init() {
 async fn main() {
     init();
 
-    let host = std::env::var("db_host").expect("Add your database address in env first");
-    let port = std::env::var("db_port").expect("Add your database port in env first");
-    let db = std::env::var("db_name").expect("Add your database port in env first");
-    let username = std::env::var("db_username").expect("Add your username in env first");
-    let password = std::env::var("db_password").expect("Add your password in env first");
-
-    // connect to database
-    let web_db = db::connect("postgres", &username, &password, &host, &port, &db).await.unwrap();
-
-    // query from database
-    let res = db::query(web_db, "select * from portfolio.songs").await.unwrap();
-
-    // print result
-    for re in res {
-        info!("Query result: {:?}",re)
-    }
-
-    // server::start().await.expect("Web server start failed");
+    server::start().await.expect("Web server start failed");
 }
