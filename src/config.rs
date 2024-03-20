812dbd94c416d84bbed792a0e9d85f68aeaa5fb9
Cloudiful <45996018+cloudiful@users.noreply.github.com>
2024-03-20 17:19:23 +0800

    enhance config.rs

--- src/config.rs
+++ src/config.rs
@@ -8,10 +8,12 @@ use serde::{Deserialize, Serialize};
 pub(crate) struct Config {
     pub(crate) mode: String,
     pub(crate) db: Database,
+    pub(crate) server: Server,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Database {
+    pub(crate) provider: String,
     pub(crate) host: String,
     pub(crate) port: String,
     pub(crate) name: String,
@@ -19,16 +21,29 @@ pub(crate) struct Database {
     pub(crate) passwd: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Server {
+    pub(crate) hostname: String,
+    pub(crate) cert: String,
+    pub(crate) cert_key: String,
+}
+
 impl Config {
     pub(crate) fn new() -> Config {
         Config {
             mode: "dev".to_string(),
             db: Database {
+                provider: "postgres".to_string(),
                 host: "your.server.host".to_string(),
                 port: "5432".to_string(),
                 name: "your database name".to_string(),
                 username: "your username".to_string(),
                 passwd: "password".to_string(),
+            },
+            server: Server {
+                hostname: "your.server.host".to_string(),
+                cert: "your-cert-name.pem".to_string(),
+                cert_key: "your-key-name.key".to_string(),
             },
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -10,29 +10,6 @@ use crate::api;
 use crate::config::Config;
 use crate::db;
 
-fn rustls_config() -> ServerConfig {
-    // load TLS key/cert files
-    let cert_file = &mut BufReader::new(File::open("portfolio.cloudiful.cn_bundle.pem").unwrap());
-    let key_file = &mut BufReader::new(File::open("portfolio.cloudiful.cn.key").unwrap());
-
-    // convert files to key/cert objects
-    let cert_chain = certs(cert_file)
-        .unwrap()
-        .into_iter()
-        .map(Certificate)
-        .collect();
-    let mut keys: Vec<PrivateKey> = rsa_private_keys(key_file)
-        .unwrap()
-        .into_iter()
-        .map(PrivateKey)
-        .collect();
-
-    ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, keys.remove(0)).unwrap()
-}
-
 // store all the service in one place
 fn services(cfg: &mut web::ServiceConfig) {
     cfg.service(api::platform_info);
@@ -47,14 +24,13 @@ fn services(cfg: &mut web::ServiceConfig) {
 pub async fn start() -> std::io::Result<()> {
     let config = Config::read();
 
-    let host = config.db.host;
-    let port = config.db.port;
-    let db = config.db.name;
-    let username = config.db.username;
-    let password = config.db.passwd;
-
     // connect to database
-    let web_db = db::connect("postgres", &username, &password, &host, &port, &db).await.unwrap();
+    let web_db = db::connect(&config.db.provider,
+                             &config.db.username,
+                             &config.db.passwd,
+                             &config.db.host,
+                             &config.db.port,
+                             &config.db.name).await.unwrap();
 
     if config.mode == "dev" {
         info!("🪜 Starting in 👷 dev mode 🛠️");
@@ -72,13 +48,32 @@ pub async fn start() -> std::io::Result<()> {
     } else {
         info!("💵 Starting in 😱 !prod! mode 🚨");
 
-        // my ssl cert
-        let config = rustls_config();
+        // load TLS key/cert files
+        let cert_file = &mut BufReader::new(File::open(config.server.cert).unwrap());
+        let key_file = &mut BufReader::new(File::open(config.server.cert_key).unwrap());
+
+        // convert files to key/cert objects
+        let cert_chain = certs(cert_file)
+            .unwrap()
+            .into_iter()
+            .map(Certificate)
+            .collect();
+        let mut keys: Vec<PrivateKey> = rsa_private_keys(key_file)
+            .unwrap()
+            .into_iter()
+            .map(PrivateKey)
+            .collect();
+
+        // my ssl cert config
+        let ssl_config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(cert_chain, keys.remove(0)).unwrap();
 
         // listen to https in prod mode
         HttpServer::new(move || {
             let cors = Cors::default()
-                .allowed_origin("https://portfolio.cloudiful.cn")
+                .allowed_origin(config.server.hostname.as_str())
                 .allowed_methods(vec!["GET", "POST"])
                 .allow_any_header()
                 .max_age(3600);
@@ -87,6 +82,6 @@ pub async fn start() -> std::io::Result<()> {
                 .wrap(cors)
                 .app_data(web::Data::new(web_db.clone()))
                 .configure(services)
-        }).bind_rustls_021(("0.0.0.0", 8080), config)?.run().await
+        }).bind_rustls_021(("0.0.0.0", 8080), ssl_config)?.run().await
     }
 }
